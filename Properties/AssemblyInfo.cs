61b7ede32c3330a0317eca848ce2a2599a2eb2c6
Ayo Adeniran <ayo10@users.noreply.github.com>
2010-10-19 16:42:01 +0000

    3.8.1 - Made CLSCompliant   - Type of LoggerSettings.BatchSize and Logger.BatchSize became int.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -9,6 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("\x00a9 Adenson.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("3.8.0.*")]
+[assembly: AssemblyVersion("3.8.1.*")]
 [assembly: AssemblyDelaySign(false)]
+[assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("ListenQuest.Data, PublicKey=0024000004800000940000000602000000240000525341310004000001000100294fcafbe66232782c3e56976ae5587a7ce8de85b96f3341227306f45c949dc9d5077f16147f19e42e738ec49df5c9361df6d2ddbe7a1c37842525af7821a9b0978f4aa2fe1f4532a4e685fabb564d7b4d9c9b25a39b4b1e6f4925c1b44e2020b3accac4a5b789451585d1580a2d08be90f69e8dc5d7f28fe327eb5f4a182ff5")]
