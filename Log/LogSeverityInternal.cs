89ea5e09c78eaac6081be1f62e4dabf54a352ed6
ayo10 <ayo10@localhost>
2011-05-28 20:47:36 +0000

    3.12 - Updates to be able to see measure writes vs other severities - Made LogEntry internal (wonder why i made it public the first instance!!!)

---
+++ Log/LogSeverityInternal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Adenson.Log
+{
+	internal struct LogSeverityInternal
+	{
+		internal static LogSeverityInternal Measure = new LogSeverityInternal { Value = "MEASURE" };
+		private string _value;
+		public LogSeverity Severity
+		{
+			get;
+			set;
+		}
+		public string Value
+		{
+			get { return _value == null ? this.Severity.ToString().ToUpper(CultureInfo.CurrentCulture) : _value; }
+			set { _value = value; }
+		}
+
+		public override string ToString()
+		{
+			return String.Concat("[", this.Value, "]").PadRight(9);
+		}
+		public static implicit operator LogSeverity(LogSeverityInternal value)
+		{
+			if (value.Value == "MEASURE") return LogSeverity.Debug;
+			return value.Severity;
+		}
+		public static implicit operator LogSeverityInternal(LogSeverity value)
+		{
+			return new LogSeverityInternal { Severity = value };
+		}
+	}
+}
