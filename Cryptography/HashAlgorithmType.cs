f52c17c174af2782157bd54258c6f4642c4ea783
Ayo Adeniran <ayo10@users.noreply.github.com>
2012-04-22 02:15:34 +0000

    1.5.8 - Updated Encryptor, added support for HMACRIPEMD160

--- Cryptography/HashAlgorithmType.cs
+++ Cryptography/HashAlgorithmType.cs
@@ -11,45 +11,50 @@ namespace Adenson.Cryptography
 		/// Perform no hash
 		/// </summary>
 		None,
-		
+
 		/// <summary>
-		/// Message-Digest algorithm 5
+		/// Message-Digest algorithm 5 (NOT RECOMMENDED FOR password hashing)
 		/// </summary>
 		MD5,
-	
+
 		/// <summary>
-		/// Secure Hash Algorithm 1
+		/// Secure Hash Algorithm 1 (NOT RECOMMENDED FOR password hashing)
 		/// </summary>
 		SHA1,
-		
+
 		/// <summary>
-		/// Secure Hash Algorithm 256
+		/// Computes a hash using the Secure Hash Algorithm 256
 		/// </summary>
 		SHA256,
-		
+
 		/// <summary>
-		/// Secure Hash Algorithm 384
+		/// Computes a hash using the Secure Hash Algorithm 384
 		/// </summary>
 		SHA384,
-		
+
 		/// <summary>
-		/// Secure Hash Algorithm 512
+		/// Computes a hash using the Secure Hash Algorithm 512
 		/// </summary>
 		SHA512,
-		
+
 		/// <summary>
-		/// RACE Integrity Primitives Evaluation Message Digest 160
+		/// Computes a hash using the RACE Integrity Primitives Evaluation Message Digest 160
 		/// </summary>
 		RIPEMD160,
-		
+
 		/// <summary>
 		/// Hash-based Message Authentication Code
 		/// </summary>
 		HMAC,
-		
+
 		/// <summary>
-		/// Message Authentication Code (MAC) using TripleDES
+		/// Computes a Message Authentication Code (MAC) using TripleDES.
 		/// </summary>
-		MACTripleDES
+		MACTripleDES,
+
+		/// <summary>
+		/// Computes a Hash-based Message Authentication Code (HMAC) using the RIPEMD160 hash function.
+		/// </summary>
+		HMACRIPEMD160
 	}
 }
