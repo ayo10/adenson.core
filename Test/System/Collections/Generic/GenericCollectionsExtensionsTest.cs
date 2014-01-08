a36edef9007a4079960a5093a5ee1fb64617bec4
Ayo Adeniran <ayo10@users.noreply.github.com>
2014-01-08 14:21:09 -0600

    1.7.6 * Removed Extensions.IsEquivalentTo (there exists a SequenceEquals in System.Core) * Updated Tests

--- Test/System/Collections/Generic/GenericCollectionsExtensionsTest.cs
+++ Test/System/Collections/Generic/GenericCollectionsExtensionsTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Adenson.CoreTest.System.Collections.Generic
@@ -27,26 +28,6 @@ namespace Adenson.CoreTest.System.Collections.Generic
 			Assert.IsTrue(dictionary.ContainsKey("one", StringComparison.CurrentCulture));
 			Assert.IsTrue(dictionary.ContainsKey("ONE", StringComparison.CurrentCultureIgnoreCase));
 			Assert.IsFalse(dictionary.ContainsKey("tHREe", StringComparison.CurrentCultureIgnoreCase));
-		}
-
-		[Test]
-		public void EqualsToTest1()
-		{
-			IEnumerable<string> array1 = new string[] { "one", "TWO", "three" };
-			IEnumerable<string> array2 = new string[] { "one", "TWO", "three" };
-			IEnumerable<string> array3 = new string[] { "ONE", "TWO", "THREE" };
-
-			Assert.IsTrue(array1.IsEquivalentTo(array2, StringComparison.CurrentCulture));
-			Assert.IsFalse(array1.IsEquivalentTo(array3, StringComparison.CurrentCulture));
-			Assert.IsTrue(array1.IsEquivalentTo(array3, StringComparison.CurrentCultureIgnoreCase));
-		}
-
-		[Test]
-		public void EqualsToTest2()
-		{
-			EqualsTest2Helper<byte>(1, 2, 3);
-			EqualsTest2Helper<string>("one", "TWO", "three");
-			EqualsTest2Helper<int>(1, 2, 3);
 		}
 
 		[Test]
@@ -87,21 +68,6 @@ namespace Adenson.CoreTest.System.Collections.Generic
 			int[] value2 = new int[] { 3, 4 };
 			int[] merged = value1.MergeWith(value2);
 			Assert.AreEqual(new int[] { 1, 2, 3, 4 }, merged);
-		}
-
-		private void EqualsTest2Helper<T>(params T[] items)
-		{
-			IEnumerable<T> array1 = new List<T>(items);
-			IEnumerable<T> array2 = new List<T>(items);
-
-			Assert.IsTrue(array1.IsEquivalentTo(array2));
-			Assert.IsTrue(array2.IsEquivalentTo(array1));
-
-			List<T> array3 = new List<T>(items);
-			array3.Add(default(T));
-
-			Assert.IsFalse(array1.IsEquivalentTo(array3));
-			Assert.IsFalse(array3.IsEquivalentTo(array1));
 		}
 	}
 }
